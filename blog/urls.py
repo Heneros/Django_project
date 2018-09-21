3a0dc80d42ed9fb79969a1a861dba12d76836c56
Rustam4457 <punk60826@gmail.com>
2018-09-22 00:27:41 +0300

    in home page added navbar

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('/blog_dev', views.about, name='blog-about'),
+    path('about/', views.about, name='blog-about'),
 ]
