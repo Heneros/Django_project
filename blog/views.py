f83c814b48c90fae38686f5c6f88efb7f37fedda
Rustam4457 <punk60826@gmail.com>
2018-09-23 01:49:53 +0300

    dictionary deleted and replacement posts from db

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,8 @@
 from django.shortcuts import render
-posts = [
-{
-	'author': 'Artur Doe',
-	'title': 'Winter Diary',
-	'content': 'First note',
-	'date_posted': 'September 21, 2018'
-},
-{
-	'author': 'Jane Moe',
-	'title': 'Summer Diary',
-	'content': 'Second note',
-	'date_posted': 'September 21, 2018'
-}
-
-]
-
+from .models import Post
 def home(request):
 	context = {
-	'posts': posts
+	'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
